09f4b8803fa9fd877d8d89dc9cb990fcd549dfea
mgechev <mgechev@gmail.com>
2015-06-02 12:09:53 +0300

    Add credentials provider

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var proxy = require('./lib/proxy');
 var express = require('express');
 var http = require('http');
 var uid = require('uid');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var ES6Promise = require('es6-promise').Promise;
 
 var app = express();
 var server = http.createServer(app);
@@ -42,13 +43,14 @@ list.add(new VNCHost({
   id: uid(5)
 }));
 
-
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/assets/'));
 
-
+app.get('/', function () {
+  // default vnc client
+});
 app.get('/admin', function (req, res) {
   res.redirect(301, '/admin/hosts/list');
 });
@@ -82,4 +84,31 @@ app.get('/admin/hosts/delete/:id', function (req, res) {
 });
 
 server.listen(8090);
+proxy.setCredentialsProvider(function (credentials) {
+  var token = credentials.token;
+  var host = list.getAll().filter(function (c) {
+    return c.writeToken === token || c.readToken === token;
+  }).pop();
+  if (host) {
+    var keyboardInterceptor;
+    var mouseInterceptor;
+    if (host.writeToken === token) {
+      keyboardInterceptor = mouseInterceptor = function () {
+        return true;
+      };
+    } else {
+      keyboardInterceptor = mouseInterceptor = function () {
+        return false;
+      };
+    }
+    return ES6Promise.resolve({
+      host: host.hostname,
+      port: host.port,
+      keyboardInterceptor: keyboardInterceptor,
+      mouseInterceptor: mouseInterceptor,
+      password: host.password
+    });
+  }
+  return ES6Promise.reject();
+});
 //proxy.init(5555);
