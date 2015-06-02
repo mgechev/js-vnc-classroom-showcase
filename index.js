a71cbd900cc72389f1b4ee55975bb163d540b289
mgechev <mgechev@gmail.com>
2015-06-02 11:23:42 +0300

    Add basic routes

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var proxy = require('./lib/proxy');
+'use strict';
 
-proxy.init(5555);
+var proxy = require('./lib/proxy');
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/views/'));
+
+app.get('/admin', function (req, res) {
+  res.redirect(301, '/admin/hosts/list');
+});
+app.get('/admin/hosts/list', function (req, res) {
+  res.render('admin', {
+    layout: 'list-hosts'
+  });
+});
+app.get('/admin/hosts/add', function (req, res) {
+  res.render('admin', {
+    layout: 'list-hosts'
+  });
+});
+
+server.listen(8090);
+//proxy.init(5555);
